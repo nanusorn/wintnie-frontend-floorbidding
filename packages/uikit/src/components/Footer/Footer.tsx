db4969218e0360687859b1f5bb19e7549b4b8513
Paul Photpipat <pphotpipat@deem.com>
2022-08-10 22:49:54 +0700

    adjust footer, remove some necessary

--- packages/uikit/src/components/Footer/Footer.tsx
+++ packages/uikit/src/components/Footer/Footer.tsx
@@ -15,7 +15,7 @@ import { FooterProps } from "./types";
 import { ThemeSwitcher } from "../ThemeSwitcher";
 import LangSelector from "../LangSelector/LangSelector";
 import CakePrice from "../CakePrice/CakePrice";
-import { LogoWithTextIcon, ArrowForwardIcon } from "../Svg";
+import { ArrowForwardIcon, WinTnieLogoWithTextIcon } from "../Svg";
 import { Button } from "../Button";
 import { Colors } from "../..";
 
@@ -34,7 +34,7 @@ const MenuItem: React.FC<FooterProps> = ({
     <StyledFooter p={["40px 16px", null, "56px 40px 32px 40px"]} {...props} justifyContent="center">
       <Flex flexDirection="column" width={["100%", null, "1200px;"]}>
         <StyledIconMobileContainer display={["block", null, "none"]}>
-          <LogoWithTextIcon isDark width="130px" />
+          <WinTnieLogoWithTextIcon isDark width="130px" />
         </StyledIconMobileContainer>
         <Flex
           order={[2, null, 1]}
@@ -66,7 +66,7 @@ const MenuItem: React.FC<FooterProps> = ({
             </StyledList>
           ))}
           <Box display={["none", null, "block"]}>
-            <LogoWithTextIcon isDark width="160px" />
+            <WinTnieLogoWithTextIcon isDark width="160px" />
           </Box>
         </Flex>
         <StyledSocialLinks order={[2]} pb={["42px", null, "32px"]} mb={["0", null, "32px"]} />
@@ -77,27 +77,13 @@ const MenuItem: React.FC<FooterProps> = ({
         >
           <Flex order={[2, null, 1]} alignItems="center">
             <ThemeSwitcher isDark={isDark} toggleTheme={toggleTheme} />
-            <LangSelector
-              currentLang={currentLang}
-              langs={langs}
-              setLang={setLang}
-              color={darkColors.textSubtle as keyof Colors}
-              dropdownPosition="top-right"
-            />
-          </Flex>
-          <Flex order={[1, null, 2]} mb={["24px", null, "0"]} justifyContent="space-between" alignItems="center">
-            <Box mr="20px">
-              <CakePrice cakePriceUsd={cakePriceUsd} color={darkColors.textSubtle as keyof Colors} />
-            </Box>
-            <Button
-              as="a"
-              href="https://pancakeswap.finance/swap?outputCurrency=0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
-              target="_blank"
-              scale="sm"
-              endIcon={<ArrowForwardIcon color={lightColors.backgroundAlt} />}
-            >
-              {buyCakeLabel}
-            </Button>
+            {/*<LangSelector*/}
+            {/*  currentLang={currentLang}*/}
+            {/*  langs={langs}*/}
+            {/*  setLang={setLang}*/}
+            {/*  color={darkColors.textSubtle as keyof Colors}*/}
+            {/*  dropdownPosition="top-right"*/}
+            {/*/>*/}
           </Flex>
         </StyledToolsContainer>
       </Flex>
